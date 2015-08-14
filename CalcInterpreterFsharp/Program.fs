5c0c9d287d129f315597d323a90b4882a1b9fdb5
Erlend Røsjø <erlend@erlend.net>
2015-08-14 14:15:59 +0200

    Whitespace and multi digit integer support

--- CalcInterpreterFsharp/Program.fs
+++ CalcInterpreterFsharp/Program.fs
@@ -5,19 +5,32 @@ type Token =
     | Plus
     | Eof
 
-let Tokenize (text: string) =
+let CharListToString list =
+    new System.String(Array.ofList list)
+
+let Tokenize (text: char list list) =
     text |> Seq.mapi(fun i x ->
         match x with
-        | x when Char.IsDigit x -> Some(Integer(Int32.Parse (sprintf "%c" x)))
-        | '+' -> Some(Plus)
-        | '\n' when i = text.Length-1 -> Some(Eof)
+        | hd :: tl when Char.IsDigit hd -> Some(Integer(x |> CharListToString |> Int32.Parse))
+        | ['+'] -> Some(Plus)
+//        | '\n' when i = text.Length-1 -> Some(Eof)
         | _ -> None
     )
 
 let Interpret tokens =
-    match tokens |> Array.ofSeq with
+    let filteredTokens = tokens |> Seq.filter (fun x -> x <> None)
+
+    match filteredTokens |> Array.ofSeq with
     | [| Some(Integer(x)); Some(Plus); Some(Integer(y)); |] -> Some(x + y)
     | _ -> None
+
+let PreprocessInput (text: string) =
+    let breakIntoDigitGroups el (acc : char list list) =
+        match el with
+        | _ when System.Char.IsDigit el -> (el :: List.head acc) :: (List.tail acc)
+        | _ -> [] :: [[el]] @ acc
+        
+    List.foldBack breakIntoDigitGroups (List.ofSeq text) [ [] ]
 
 [<EntryPoint>]
 let main argv = 
@@ -25,7 +38,7 @@ let main argv =
         do
             printf "Input x+y expression: "
             let input = System.Console.ReadLine()
-            let result = input |> Tokenize |> Interpret
+            let result = input |> PreprocessInput |> Tokenize |> Interpret
             match result with
             | Some result -> printfn "Result: %i" result
             | None -> printfn "Could not parse, try again"
